ea951231fd6262437f8e77b245f51f354a4476bb
Ignacio Glinsek <iglinsek@gmail.com>
2018-07-17 20:19:38 -0300

    Added credit card validation and auth 0 authentication

---
+++ TestCreditCard.Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TestCreditCard.Web.Models;
+
+namespace TestCreditCard.Web.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
